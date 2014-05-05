feeeb66733ccbdf563518ce03ecf55c45dad2299
Kent Stark Olsen <kent.stark.olsen@gmail.com>
2014-05-05 20:37:18 +0200

    point_cloud_assembler: Implemented stitching filter

--- modelling/point_cloud_assembler/src/PointCloudStitching.cpp
+++ modelling/point_cloud_assembler/src/PointCloudStitching.cpp
@@ -7,12 +7,83 @@
 
 #include "PointCloudStitching.hpp"
 
-PointCloudStitching::PointCloudStitching() {
-	// TODO Auto-generated constructor stub
+PointCloudStitching::PointCloudStitching()
+{
+	this->reset();
+}
+
+PointCloudStitching::~PointCloudStitching()
+{
 
 }
 
-PointCloudStitching::~PointCloudStitching() {
-	// TODO Auto-generated destructor stub
+void PointCloudStitching::reset(void)
+{
+	this->stitching = pcl::PointCloud<PointT>();
 }
 
+pcl::PointCloud<PointT>::ConstPtr PointCloudStitching::getStitching(void)
+{
+	return this->stitching.makeShared();
+}
+
+int PointCloudStitching::stitch(pcl::PointCloud<PointT>& points)
+{
+	if (this->stitching.size() == 0)
+	{
+		pcl::copyPointCloud(points, this->stitching);
+		return 0;
+	}
+
+	//	Compute surface normals and curvature
+	pcl::PointCloud<PointT> tempTarget;
+	pcl::copyPointCloud(this->stitching, tempTarget);
+
+	pcl::PointCloud<pcl::PointNormal>::Ptr pointsWithNormalSource (new pcl::PointCloud<pcl::PointNormal>);
+	pcl::PointCloud<pcl::PointNormal>::Ptr pointsWithNormalTarget (new pcl::PointCloud<pcl::PointNormal>);
+
+	pcl::NormalEstimation<PointT, pcl::PointNormal> normalEstimate;
+	pcl::search::KdTree<PointT>::Ptr tree (new pcl::search::KdTree<PointT> ());
+	normalEstimate.setSearchMethod(tree);
+	normalEstimate.setKSearch(30);
+
+	normalEstimate.setInputCloud(points.makeShared());
+	normalEstimate.compute(*pointsWithNormalSource);
+	pcl::copyPointCloud(points, *pointsWithNormalSource);
+
+	normalEstimate.setInputCloud (tempTarget.makeShared());
+	normalEstimate.compute(*pointsWithNormalTarget);
+	pcl::copyPointCloud (tempTarget, *pointsWithNormalTarget);
+
+	//	Instantiate custom point representation
+	MyPointNormal pointNormal;
+	//	... and weight the 'curvature' dimension so that it is balanced against x, y, and z
+	float alpha[4] = {1.0, 1.0, 1.0, 1.0};
+	pointNormal.setRescaleValues(alpha);
+
+	//	Align
+	pcl::IterativeClosestPointNonLinear<pcl::PointNormal, pcl::PointNormal> registration;
+	registration.setTransformationEpsilon(1e-5);
+	//	Set the maximum distance between two correspondences
+	registration.setMaxCorrespondenceDistance(0.02);
+	//	Set the point representation
+	registration.setPointRepresentation (boost::make_shared<const MyPointNormal> (pointNormal));
+
+	registration.setInputSource(pointsWithNormalSource);
+	registration.setInputTarget(pointsWithNormalTarget);
+	registration.setMaximumIterations(30);
+
+	PCL_INFO("Sizes:  %d  --  %d", (int)pointsWithNormalSource.get()->size(), (int)pointsWithNormalTarget.get()->size());
+
+	Eigen::Matrix4f tf = Eigen::Matrix4f::Identity();
+	pcl::PointCloud<pcl::PointNormal>::Ptr regResult = pointsWithNormalSource;
+	registration.align(*regResult);
+	tf = registration.getFinalTransformation().inverse();
+
+	pcl::transformPointCloud(*pointsWithNormalTarget, *regResult, tf);
+	*regResult += *pointsWithNormalSource;
+
+	pcl::copyPointCloud(*regResult, this->stitching);
+
+	return 0;
+}
--- modelling/point_cloud_assembler/src/PointCloudStitching.hpp
+++ modelling/point_cloud_assembler/src/PointCloudStitching.hpp
@@ -18,40 +18,23 @@
 #include <pcl/registration/icp_nl.h>
 #include <pcl/registration/transforms.h>
 
+#include "MyPointNormal.hpp"
+
 typedef pcl::PointXYZ PointT;
-typedef pcl::PointCloud<PointT> PointCloud;
-typedef pcl::PointNormal PointNormalT;
-typedef pcl::PointCloud<PointNormalT> PointCloudWithNormals;
 
-class PointNormal : public pcl::PointRepresentation <pcl::PointNormal>
-{
-	using pcl::PointRepresentation<pcl::PointNormal>::nr_dimensions_;
-
-public:
-	PointNormal ()
-  	{
-		//	Define the number of dimensions
-		nr_dimensions_ = 4;
-  	}
-
-	//	Override the copyToFloatArray method to define feature vector
-	virtual void copyToFloatArray (const pcl::PointNormal& p, float *out) const
-	{
-		// < x, y, z, curvature >
-		out[0] = p.x;
-		out[1] = p.y;
-		out[2] = p.z;
-		out[3] = p.curvature;
-	}
-};
-
-template <class _type> class PointCloudStitching
+class PointCloudStitching
 {
 private:
+	pcl::PointCloud<PointT> stitching;
+
+	void reset(void);
 
 public:
 	PointCloudStitching();
 	virtual ~PointCloudStitching();
+
+	pcl::PointCloud<PointT>::ConstPtr getStitching(void);
+	int stitch(pcl::PointCloud<PointT>& points);
 };
 
 #endif /* POINTCLOUDSTITCHING_HPP_ */
