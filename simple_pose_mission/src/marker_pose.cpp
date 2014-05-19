ce94a2be6b6c3989ee0f72e2e091b7366c2f60b0
dimx.dk <dimx@dimx.dk>
2014-05-19 19:37:24 +0200

    Corrent files for marker class in simple_pose_mission added

--- simple_pose_mission/src/marker_pose.cpp
+++ simple_pose_mission/src/marker_pose.cpp
@@ -3,9 +3,9 @@
 namespace vm = visualization_msgs;
 namespace im = interactive_markers;
 
-MarkerPose::MarkerPose(std::string marker_server_name) : marker_server(marker_server_name), default_frame_id("/base_link")
+MarkerPose::MarkerPose(std::string marker_server_name) : marker_server(marker_server_name), default_frame_id("/base_link"), nh()
 {
-
+    tf_update_timer = nh.createTimer(ros::Duration(1), boost::bind(&MarkerPose::updateTFCallback,this,_1));
 }
 
 void MarkerPose::add6DOFBoxMarker(std::string name, std::string description, im::InteractiveMarkerServer::FeedbackCallback callback)
@@ -25,6 +25,26 @@ void MarkerPose::add6DOFBoxMarker(std::string name, std::string description, im:
     marker_server.applyChanges();
 
     ROS_INFO("6DOF marker added");
+}
+
+void MarkerPose::add6DOFBoxMarkerTF(std::string name, std::string description, tf::Pose init_pose)
+{
+    vm::InteractiveMarker interactive_marker = getDefaultMarker(name, description);
+    interactive_marker.pose = tfPoseToGeometryPose(init_pose);
+
+    addDefaultBoxMarker(interactive_marker);
+    add6DOFControl(interactive_marker);
+
+    marker_pose_list[name] = init_pose;
+
+    marker_server.insert(interactive_marker);
+    marker_server.setCallback(name, boost::bind(&MarkerPose::markerFeedback,this,_1));
+    marker_server.applyChanges();
+}
+
+tf::Pose MarkerPose::getPose(std::string name)
+{
+    return marker_pose_list[name];
 }
 
 vm::InteractiveMarker MarkerPose::getDefaultMarker(std::string name, std::string description)
@@ -100,4 +120,49 @@ void MarkerPose::add6DOFControl(vm::InteractiveMarker &marker)
     control.name = "move_y";
     control.interaction_mode = vm::InteractiveMarkerControl::MOVE_AXIS;
     marker.controls.push_back(control);
+}
+
+tf::Pose MarkerPose::geometryPoseToTfPose(const geometry_msgs::Pose gPose) const
+{
+    tf::Pose tfPose;
+    tfPose.setOrigin(tf::Vector3(gPose.position.x,gPose.position.y,gPose.position.z));
+    tfPose.setRotation(tf::Quaternion(gPose.orientation.x, gPose.orientation.y, gPose.orientation.z, gPose.orientation.w));
+    return tfPose;
+}
+
+geometry_msgs::Pose MarkerPose::tfPoseToGeometryPose(const tf::Pose tPose) const
+{
+    geometry_msgs::Pose gPose;
+    gPose.position.x = tPose.getOrigin().x();
+    gPose.position.y = tPose.getOrigin().y();
+    gPose.position.z = tPose.getOrigin().z();
+    gPose.orientation.x = tPose.getOrigin().x();
+    gPose.orientation.y = tPose.getOrigin().y();
+    gPose.orientation.z = tPose.getOrigin().z();
+    gPose.orientation.w = tPose.getOrigin().w();
+
+    return gPose;
+}
+
+void MarkerPose::markerFeedback(const visualization_msgs::InteractiveMarkerFeedbackConstPtr &feedback)
+{
+    //transform_pose.find(feedback->marker_name)->second = this->geometryPoseToTfPose(feedback->pose);
+    marker_pose_list[feedback->marker_name] = this->geometryPoseToTfPose(feedback->pose);
+    /*
+    tf::Transform transform;
+    tf::Pose pose = this->geometryPoseToTfPose(feedback->pose);
+    transform.setOrigin(pose.getOrigin());
+    transform.setRotation(pose.getRotation());
+    transform_broadcaster.sendTransform(tf::StampedTransform(transform, ros::Time::now(), feedback->header.frame_id, feedback->marker_name));
+    */
+}
+
+void MarkerPose::updateTFCallback(const ros::TimerEvent &)
+{
+    for (marker_pose_list_type::iterator it=marker_pose_list.begin(); it!=marker_pose_list.end(); ++it)
+    {
+        const std::string& marker_name = it->first;
+        const tf::Pose& pose = it->second;
+        transform_broadcaster.sendTransform(tf::StampedTransform(pose, ros::Time::now(), default_frame_id, marker_name));
+    }
 }
--- simple_pose_mission/src/marker_pose.h
+++ simple_pose_mission/src/marker_pose.h
@@ -3,21 +3,39 @@
 
 #include <string>
 #include <interactive_markers/interactive_marker_server.h>
+#include <tf/transform_broadcaster.h>
+#include <boost/bind.hpp>
+#include <map>
 
 class MarkerPose
 {
 public:
     MarkerPose(std::string marker_server_name);
     void add6DOFBoxMarker(std::string name, std::string description, interactive_markers::InteractiveMarkerServer::FeedbackCallback callback);
+    void add6DOFBoxMarkerTF(std::string name, std::string description, tf::Pose init_pose);
+    tf::Pose getPose(std::string name);
 
 protected:
+    ros::NodeHandle nh;
+
     interactive_markers::InteractiveMarkerServer marker_server;
     std::string default_frame_id;
+    tf::TransformBroadcaster transform_broadcaster;
 
     visualization_msgs::InteractiveMarker getDefaultMarker(std::string name, std::string description);
     visualization_msgs::Marker getDefaultBoxMarker();
     void addDefaultBoxMarker(visualization_msgs::InteractiveMarker &marker);
     void add6DOFControl(visualization_msgs::InteractiveMarker &marker);
+
+    tf::Pose geometryPoseToTfPose(const geometry_msgs::Pose gPose) const;
+    geometry_msgs::Pose tfPoseToGeometryPose(const tf::Pose tPose) const;
+
+    // TF
+    typedef std::map<std::string, tf::Pose> marker_pose_list_type;
+    ros::Timer tf_update_timer;
+    marker_pose_list_type marker_pose_list;
+    void markerFeedback(const visualization_msgs::InteractiveMarkerFeedbackConstPtr &feedback );
+    void updateTFCallback(const ros::TimerEvent&);
 };
 
 #endif // MARKER_POSE_H
